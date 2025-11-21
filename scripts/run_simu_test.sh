28a37c681daeac14882714aba7e17aa6f8c7cd39
chlience <chlience@outlook.com>
2025-11-21 07:11:16 +0000

    update experiment 1

--- scripts/run_simu_test.sh
+++ scripts/run_simu_test.sh
@@ -10,6 +10,6 @@ mv logs_simu/fifo_simulation_seed_26_task_20.log logs_simu/yarn-cs_simulation_0.
 
 python3 examples/extract_log_runtime.py --log_dir logs_simu
 
-python3 script/parse_log.py logs_simu/elasgnn.log --output logs_simu/elasgnn.json
-python3 script/parse_log.py logs_simu/sjf.log --output logs_simu/sjf.json
-python3 script/parse_log.py logs_simu/yarn-cs.log --output logs_simu/yarn-cs.json
+python3 script/parse_log.py logs_simu/elasgnn_simulation_0.log --output logs_simu/elasgnn.json
+python3 script/parse_log.py logs_simu/sjf_simulation_0.log --output logs_simu/sjf.json
+python3 script/parse_log.py logs_simu/yarn-cs_simulation_0.log --output logs_simu/yarn-cs.json
